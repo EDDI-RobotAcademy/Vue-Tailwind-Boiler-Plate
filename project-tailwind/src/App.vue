cdfc3b592262158cbe66c5add02c70834320ecb1
ekankr2 <ekankr2@naver.com>
2023-01-09 17:21:43 +0900

    chore: add normalize.css

--- project-tailwind/src/App.vue
+++ project-tailwind/src/App.vue
@@ -5,7 +5,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header class="leading-normal max-h-screen">
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="flex justify-start flex-wrap">
       <HelloWorld msg="You did it!" />
