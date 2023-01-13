75ca8091fa4672b6b96e94724712cd326e18a588
ekankr2 <ekankr2@naver.com>
2023-01-13 09:46:44 +0900

    feat: create mainLayout component and add it to global component
    - add main layout to global component
    - add layout type in meta to use it in component

---
+++ board-with-tailwind/src/components/layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import MainHeader from '../sections/MainHeader.vue'
+import Footer from "@/components/sections/Footer.vue";
+</script>
+
+<template>
+  <MainHeader/>
+  <slot/>
+  <Footer/>
+</template>
+
+<script>
+export default {}
+</script>
