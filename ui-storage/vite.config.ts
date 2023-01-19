2733f528247f3be5d728f90a52ff44b58676f928
ekankr2 <ekankr2@naver.com>
2023-01-19 19:00:19 +0900

    file: remove App.vue, main.ts and replace it with histoire.setup.ts

--- ui-storage/vite.config.ts
+++ ui-storage/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="histoire" />
+
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -6,6 +8,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  histoire: {
+    setupFile: '/src/histoire.setup.ts',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
