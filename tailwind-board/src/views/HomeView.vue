12334a8b9dd4eddba31d4c33c7492ccf2251d516
ekank <ekankr2@naver.com>
2023-01-23 21:08:52 +0900

    chore: add TailwindCSS to project

--- tailwind-board/src/views/HomeView.vue
+++ tailwind-board/src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main><h2 class="bg-green-500">hello world</h2>
   </main>
 </template>
